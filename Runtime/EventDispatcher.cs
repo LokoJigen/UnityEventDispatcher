5231396db00b968f3eb9084423303d1e643a171a
GinoV <ginovalerio123@gmail.com>
2025-07-11 18:53:09 +0200

    modified EventDispatcher.cs - moved to single Delegate and callbacks on state changes

--- Runtime/EventDispatcher.cs
+++ Runtime/EventDispatcher.cs
@@ -4,68 +4,127 @@ using UnityEngine;
 
 namespace com.lokojigen.events
 {
+    public enum CallbackFailureReason
+    {
+        Unknown,
+        UnsubscriptionMissingDelegate,
+        UnsubscriptionMissingEventType,
+        AlreadySubscribed,
+    }
+
     /// <summary>
-    /// Use this implementation for internal app communication with typed events.
+    /// EventDispatcher is a Unity MonoBehaviour that allows you to manage events in a type-safe manner.
+    /// It supports subscribing, unsubscribing, and triggering events with callbacks.
+    /// It also provides events to notify subscribers about changes in the event dispatcher state and failures in subscription, unsubscription, or triggering.
+    /// BE AWARE: This class is not thread-safe and should be used in the main Unity thread only.
+    /// BE AWARE: This class does not support duplicate subscriptions, meaning if you try to subscribe the same callback multiple times, it will fail and notify the failure event.
+    /// BE AWARE: The delegates subscription list order matters for the order of execution when triggering events. 
     /// </summary>
     public class EventDispatcher : MonoBehaviour
     {
-        // Contains all available events types
-        private Dictionary<Type, List<Delegate>> eventDictionary = new();
+        private Dictionary<Type, Delegate> eventTable = new();
 
-        public static IEventLogger Logger { get; set; }
+        // Events to notify subscribers about changes in the event dispatcher state
+        public event Action<Type, Delegate> OnSubscribed;
+        public event Action<Type, Delegate> OnUnsubscribed;
+        public event Action<Type, BaseEvent> OnTrigger;
+
+        // Events to notify subscribers about failures in subscription, unsubscription, or triggering
+        public event Action<Type, Delegate, CallbackFailureReason> OnCallbackFailure;
+        public event Action<Type, BaseEvent> OnTriggerFailure;
 
         /// <summary>
-        /// Add the callback to T event when triggered.
+        /// SubscribeToEvent allows you to register a callback for a specific event type.
+        /// If the event type is already registered, it will notify the failed subscription event with a reason.
         /// </summary>
-        /// <typeparam name="T">Type of Event</typeparam>
-        /// <param name="callback">Method to call when event T is triggered</param>
         public void SubscribeToEvent<T>(Action<T> callback) where T : BaseEvent
         {
-            Type eventType = typeof(T);
-            if (!eventDictionary.ContainsKey(eventType))
+            var type = typeof(T);
+
+            if (eventTable.TryGetValue(type, out var existingDelegate))
             {
-                eventDictionary[eventType] = new List<Delegate>();
-            }
-
-            eventDictionary[eventType].Add(callback);
-
-            Logger?.Log($"[EventManager] Subscribed: {callback.Method.Name} from {callback.Target}");
-        }
-
-        /// <summary>
-        /// Unsubscribe callback from T event.
-        /// </summary>
-        /// <typeparam name="T">Type of event</typeparam>
-        /// <param name="callback">Method to unsubscribe from event T</param>
-        public void UnsubscribeToEvent<T>(Action<T> callback) where T : BaseEvent
-        {
-            Type eventType = typeof(T);
-            if (eventDictionary.ContainsKey(eventType))
-            {
-                eventDictionary[eventType].Remove(callback);
-                Logger?.Log($"[EventManager] Unsubscribed: {callback.Method.Name} from {callback.Target}");
-            }
-        }
-
-        /// <summary>
-        /// Trigger all callbacks stored for event T, if any.
-        /// </summary>
-        /// <typeparam name="T">Type of event</typeparam>
-        /// <param name="targetEvent">Event to trigger</param>
-        public void TriggerEvent<T>(T targetEvent) where T : BaseEvent
-        {
-            Type eventType = typeof(T);
-            if (eventDictionary.ContainsKey(eventType))
-            {
-                Logger?.Log($"[EventManager] TriggerEvent - {targetEvent.GetType()}");
-
-                for (int i = 0; i < eventDictionary[eventType].Count; i++)
+                foreach (var del in existingDelegate.GetInvocationList())
                 {
-                    if (eventDictionary[eventType][i] is Action<T> action)
+                    if (del == (Delegate)callback)
                     {
-                        action.Invoke(targetEvent);
+                        OnCallbackFailure?.Invoke(type, callback, CallbackFailureReason.AlreadySubscribed);
+                        return; // Already registered
                     }
                 }
+
+                eventTable[type] = Delegate.Combine(existingDelegate, callback);
+            }
+            else
+            {
+                eventTable[type] = callback;
+            }
+
+            OnSubscribed?.Invoke(type, callback);
+        }
+
+        /// <summary>
+        /// UnsubscribeToEvent allows you to remove a previously registered callback for a specific event type.
+        /// If the callback is not found, it will notify the failed unsubscription event with a reason.
+        /// </summary>
+        public void UnsubscribeToEvent<T>(Action<T> callback) where T : BaseEvent
+        {
+            var type = typeof(T);
+
+            if (eventTable.TryGetValue(type, out var existingDelegate))
+            {
+                var invocationList = existingDelegate.GetInvocationList();
+                bool found = false;
+
+                foreach (var del in invocationList)
+                {
+                    if (del == (Delegate)callback)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    OnCallbackFailure?.Invoke(type, callback, CallbackFailureReason.UnsubscriptionMissingDelegate);
+                    return;
+                }
+
+                // Remove only the first occurrence (safe, since duplicate subscriptions are no longer allowed)
+                var newDelegate = Delegate.Remove(existingDelegate, callback);
+
+                if (newDelegate == null)
+                {
+                    eventTable.Remove(type);
+                }
+                else
+                {
+                    eventTable[type] = newDelegate;
+                }
+
+                OnUnsubscribed?.Invoke(type, callback);
+            }
+            else
+            {
+                OnCallbackFailure?.Invoke(type, callback, CallbackFailureReason.UnsubscriptionMissingEventType);
+            }
+        }
+
+        /// <summary>
+        /// TriggerEvent allows you to invoke all callbacks registered for a specific event type.
+        /// If no callbacks are registered, it will notify the trigger failure event.
+        /// </summary>
+        public void TriggerEvent<T>(T evt) where T : BaseEvent
+        {
+            var type = typeof(T);
+            if (eventTable.TryGetValue(type, out var del) && del is Action<T> action)
+            {
+                OnTrigger?.Invoke(type, evt);
+                action.Invoke(evt);
+            }
+            else
+            {
+                OnTriggerFailure?.Invoke(type, evt);
             }
         }
     }
